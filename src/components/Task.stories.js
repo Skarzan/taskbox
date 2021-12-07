cffa6a368c7792f4d0c13987ed524ba40a7f7a15
Christian Kohls <christian.kohls01@sap.com>
2021-12-07 08:47:46 +0100

    reverted red background and add longText story

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -34,3 +34,11 @@ Archived.args = {
     state: 'TASK_ARCHIVED',
   }
 };
+
+export const longStringTitle = Template.bind({});
+longStringTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+  }
+};
